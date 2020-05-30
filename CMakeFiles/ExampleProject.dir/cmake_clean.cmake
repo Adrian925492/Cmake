28c9470ad7d41ac01511be92f0204efa769830a9
Adrian925492 <adrian.podsiadlowski@gmail.com>
2020-05-30 12:06:05 +0200

    cmake added

---
+++ CMakeFiles/ExampleProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ExampleProject.dir/main.cpp.o"
+  "ExampleProject"
+  "ExampleProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ExampleProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
