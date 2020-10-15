6feca17c6ec486b719d30edebe421b472c6864fb
Adrian Podsiadlowski <adrian.9254@gmail.com>
2020-10-15 17:18:46 +0000

    sources added

---
+++ train1_mathlib/add.c
@@ -0,0 +1,6 @@
+#include "add.h"
+
+int plus_def(int x, int y) noexcept
+{
+    return x + y;
+}
---
+++ train1_mathlib/add.h
@@ -0,0 +1,14 @@
+#ifndef ADD_H
+#define ADD_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int plus_def(int x, int y) noexcept;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
