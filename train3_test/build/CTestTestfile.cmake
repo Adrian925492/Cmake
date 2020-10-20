35251845115a021927a2ef929e567c021184b434
Adrian Podsiadlowski <adrian.9254@gmail.com>
2020-10-20 19:30:14 +0000

    test data myk

--- train3_test/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /repo/Cmake/train3_test
-# Build directory: /repo/Cmake/train3_test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("add")
-subdirs("substract")
-subdirs("test")
--- train3_test/build/add/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /repo/Cmake/train3_test/add
-# Build directory: /repo/Cmake/train3_test/build/add
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
